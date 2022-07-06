d896686bd112c3a991696daa32d7c5036b294d80
kashifpeer <mail.qashifpeer@gmail.com>
2022-07-06 13:20:42 +0530

    SECTION FACTS

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function() {
+    /*----------------------------Navbar Shrink------*/
+    $(window).on("scroll",function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+
+
+        }
+
+    });
+    /*--------------------- Video Popup--------------*/
+    const videoScr = $("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click", function(){
+       if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src","")
+       }
+       else{
+            $(".video-popup").addClass("open");
+            if($("#player-1").attr("src")==''){
+            $("player-1").attr("src",videoScr);
+        }
+       }
+    });
+    /*-------------------------Features Carousal-----*/
+
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+               
+            },
+            600:{
+                items:2,
+             
+            },
+            1000:{
+                items:3,
+               
+            }
+        }
+    });
+});
