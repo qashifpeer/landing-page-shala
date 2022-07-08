5d2363f4ef28bc2da3aa298415a20e863aeb31fa
kashifpeer <mail.qashifpeer@gmail.com>
2022-07-08 08:12:16 +0530

    08-july

--- js/main.js
+++ js/main.js
@@ -27,11 +27,53 @@ $(document).ready(function() {
        }
     });
     /*-------------------------Features Carousal-----*/
-
     $('.features-carousel').owlCarousel({
         loop:true,
         margin:0,
         autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+               
+            },
+            600:{
+                items:2,
+             
+            },
+            1000:{
+                items:3,
+               
+            }
+        }
+    });
+
+    /*-------------------------screenshots Carousal-----*/
+    $('.screenshots-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+               
+            },
+            600:{
+                items:2,
+             
+            },
+            1000:{
+                items:3,
+               
+            }
+        }
+    });
+    /*-------------------------Testimonial Carousal-----*/
+    $('.testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        // autoplay:true,
         responsiveClass:true,
         responsive:{
             0:{
