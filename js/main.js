94f38d458b93941a467a06cdc2d7ff38f604fe48
kashifpeer <mail.qashifpeer@gmail.com>
2022-07-11 18:40:49 +0530

    faq section complete\

--- js/main.js
+++ js/main.js
@@ -90,4 +90,25 @@ $(document).ready(function() {
             }
         }
     });
+    /*-------------------------Team Carousal-----*/
+    $('.team-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+               
+            },
+            600:{
+                items:2,
+             
+            },
+            1000:{
+                items:3,
+               
+            }
+        }
+    });
 });
